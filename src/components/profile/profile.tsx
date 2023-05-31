31dbe2901fc4dacd69a9fbbb02e8dda633a7ac29
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-30 17:31:45 -0700

    create and use IProfile interface to define the prop types

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,7 +1,12 @@
 import React, { FC, ReactElement } from 'react';
 import { Avatar, Box, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
 
-const Profile: FC = (): ReactElement => {
+interface IProfile {
+  name?: string;
+}
+
+const Profile: FC<IProfile> = ({ name }): ReactElement => {
   return (
     <Box
       sx={{
@@ -20,12 +25,12 @@ const Profile: FC = (): ReactElement => {
         }}
       >
         <Typography variant="h4" color="text.primary">
-          J
+          {name.substring(0, 1)}
         </Typography>
       </Avatar>
 
       <Typography variant="h6" color="text.primary">
-        Welcome, John!
+        {`Welcome, ${name}!`}
       </Typography>
       <Typography variant="body1" color="text.primary">
         This is your personal tasks manager
@@ -35,3 +40,7 @@ const Profile: FC = (): ReactElement => {
 };
 
 export default Profile;
+
+Profile.propTypes = {
+  name: PropTypes.string,
+};
