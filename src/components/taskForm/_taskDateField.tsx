4a7fd1baf7a0fd89dd2022d43f28da733d959cd7
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-31 16:01:27 -0700

    finalize the TaskDateField component

--- src/components/taskForm/_taskDateField.tsx
+++ src/components/taskForm/_taskDateField.tsx
@@ -1,7 +1,23 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
+import { TextField } from '@mui/material';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
 const TaskDateField: FC = (): ReactElement => {
-  return ()
-}
+  const [date, setDate] = React.useState<Date | null>(null);
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DesktopDatePicker
+        label="Task Date"
+        format="dd/MM/yyyy"
+        value={date}
+        onChange={(newValue) => {
+          setDate(newValue);
+        }}
+      />
+    </LocalizationProvider>
+  );
+};
 
 export default TaskDateField;
