f7eca1558db89b3d4a160022dbbeca6854fbedbc
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-30 17:07:18 -0700

    add hardcoded values to the Profile component

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,9 +1,37 @@
 import React, { FC, ReactElement } from 'react';
+import { Avatar, Box, Typography } from '@mui/material';
 
 const Profile: FC = (): ReactElement => {
   return (
-    <h3>Profile</h3>
-  )
-}
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar
+        sx={{
+          width: '96px',
+          height: '96px',
+          backgroundColor: 'primary.main',
+          marginBottom: '16px',
+        }}
+      >
+        <Typography variant="h4" color="text.primary">
+          J
+        </Typography>
+      </Avatar>
+
+      <Typography variant="h6" color="text.primary">
+        Welcome, John!
+      </Typography>
+      <Typography variant="body1" color="text.primary">
+        This is your personal tasks manager
+      </Typography>
+    </Box>
+  );
+};
 
 export default Profile;
