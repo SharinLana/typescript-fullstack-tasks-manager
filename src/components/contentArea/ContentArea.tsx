9f1df243e3b83e9d8e31846e0efd28132effeaa3
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-31 19:17:33 -0700

    modify the header in the ContentArea

--- src/components/contentArea/ContentArea.tsx
+++ src/components/contentArea/ContentArea.tsx
@@ -1,10 +1,16 @@
 import React, { FC, ReactElement } from 'react';
-import { Grid } from '@mui/material';
+import { format } from 'date-fns';
+import { Grid, Box } from '@mui/material';
 
 const ContentArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h2>Content Area</h2>
+      <Box mb={8} px={4}>
+        <h2>
+          Status Of Your Tasks As On{' '}
+          {format(new Date(), 'PPPP')}
+        </h2>
+      </Box>
     </Grid>
   );
 };
