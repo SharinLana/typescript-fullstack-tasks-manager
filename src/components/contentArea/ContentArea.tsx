ed43607cfcb328a3681c2d925ca435e6b3d26497
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-31 19:25:04 -0700

    change the fontWeight of the header

--- src/components/contentArea/ContentArea.tsx
+++ src/components/contentArea/ContentArea.tsx
@@ -6,15 +6,26 @@ const ContentArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
       <Box mb={8} px={4}>
-        <h2>Status Of Your Tasks As On {format(new Date(), 'PPPP')}</h2>
+        <h2 style={{ fontWeight: 400 }}>
+          Status Of Your Tasks As On {format(new Date(), 'PPPP')}
+        </h2>
       </Box>
       <Grid container display="flex" justifyContent="center">
-        <Grid>
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10}
+          xs={12}
+          mb={8}
+        >
           <Box>Task Counter</Box>
           <Box>Task Counter</Box>
           <Box>Task Counter</Box>
         </Grid>
-        <Grid>
+        <Grid item display="flex" flexDirection="column" xs={10} md={8}>
           <Box>Tasks </Box>
           <Box>Tasks </Box>
         </Grid>
