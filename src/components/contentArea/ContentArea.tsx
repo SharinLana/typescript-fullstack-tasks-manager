7592a354807da3b3a0ec2e727a745bc569beb87a
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-31 19:20:06 -0700

    add a new container to the ContentArea

--- src/components/contentArea/ContentArea.tsx
+++ src/components/contentArea/ContentArea.tsx
@@ -6,11 +6,19 @@ const ContentArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
       <Box mb={8} px={4}>
-        <h2>
-          Status Of Your Tasks As On{' '}
-          {format(new Date(), 'PPPP')}
-        </h2>
+        <h2>Status Of Your Tasks As On {format(new Date(), 'PPPP')}</h2>
       </Box>
+      <Grid container display="flex" justifyContent="center">
+        <Grid>
+          <Box>Task Counter</Box>
+          <Box>Task Counter</Box>
+          <Box>Task Counter</Box>
+        </Grid>
+        <Grid>
+          <Box>Tasks </Box>
+          <Box>Tasks </Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
