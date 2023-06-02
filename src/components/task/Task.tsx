3cd9bec577d8960c476b7c9d357ab904bbae4677
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-02 14:12:56 -0700

    create a new directory for rendering tasks

---
+++ src/components/task/Task.tsx
@@ -0,0 +1,8 @@
+import React, { FC, ReactElement } from 'react';
+import { Box } from '@mui/material';
+
+const Task: FC = (): ReactElement => {
+  return <Box>Test</Box>;
+};
+
+export default Task;
