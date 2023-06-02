0d6e81654717b088028f199235bdca127f70d953
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-02 13:22:57 -0700

    apply styles to the Material UI  components

--- src/components/taskCounter/TaskCounter.tsx
+++ src/components/taskCounter/TaskCounter.tsx
@@ -10,10 +10,28 @@ const TaskCounter: FC = (): ReactElement => {
         justifyContent="center"
         alignItems="center"
       >
-        <Avatar>
-          <Typography>10</Typography>
+        <Avatar
+          sx={{
+            backgroundColor: 'transparent',
+            border: '5px solid',
+            width: '96px',
+            height: '96px',
+            marginBottom: '16px',
+            borderColor: 'warning.light',
+          }}
+        >
+          <Typography variant="h4" color="#fffff">
+            10
+          </Typography>
         </Avatar>
-        <Typography>Subtitle</Typography>
+        <Typography
+          color="#ffff"
+          fontWeight="bold"
+          fontSize="20px"
+          variant="h5"
+        >
+          Subtitle
+        </Typography>
       </Box>
     </>
   );
