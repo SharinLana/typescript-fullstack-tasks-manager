e56741ae7dd73cdda58aec82e9661246e6fc03aa
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-02 14:24:27 -0700

    apply styles to the Task component

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -2,7 +2,25 @@ import React, { FC, ReactElement } from 'react';
 import { Box } from '@mui/material';
 
 const Task: FC = (): ReactElement => {
-  return <Box>Test</Box>;
+  return (
+    <Box
+      display="flex"
+      width="100%"
+      justifyContent="flex-start"
+      flexDirection="column"
+      mb={2}
+      p={4}
+      sx={{
+        width: '100%',
+        backgroundColor: 'background.paper',
+        borderRadius: '8px',
+        border: '1px solid',
+        borderColor: 'error.light',
+      }}
+    >
+      Test
+    </Box>
+  );
 };
 
 export default Task;
