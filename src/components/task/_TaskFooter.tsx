e582ffae82179c8b8ca20da85e9d83d1a309cce5
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-02 15:40:20 -0700

    add styles to the TaskFooter component

--- src/components/task/_TaskFooter.tsx
+++ src/components/task/_TaskFooter.tsx
@@ -3,12 +3,24 @@ import { Box, Button, Switch, FormControlLabel } from '@mui/material';
 
 const TaskFooter: FC = (): ReactElement => {
   return (
-    <Box>
+    <Box
+      display="flex"
+      width="100%"
+      justifyContent="space-between"
+      mt={4}
+    >
       <FormControlLabel
         label="In Progress"
         control={<Switch color="warning" />}
       />
-      <Button>Mark Complete</Button>
+      <Button
+        variant="contained"
+        color="success"
+        size="small"
+        sx={{ color: 'white' }}
+      >
+        Mark Complete
+      </Button>
     </Box>
   );
 };
