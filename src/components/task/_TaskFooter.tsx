36efafe3699992c0242adc38e736dfd5c216aa03
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-02 16:11:37 -0700

    use ITaskFooter props to handle events in the TaskFooter component

--- src/components/task/_TaskFooter.tsx
+++ src/components/task/_TaskFooter.tsx
@@ -1,16 +1,18 @@
 import React, { FC, ReactElement } from 'react';
 import { Box, Button, Switch, FormControlLabel } from '@mui/material';
+import { ITaskFooter } from './interfaces/ITaskFooter';
 
-const TaskFooter: FC = (): ReactElement => {
+const TaskFooter: FC<ITaskFooter> = (props): ReactElement => {
+  const {
+    onStatusChange = (e) => console.log(e),
+    onClick = (e) => console.log(e),
+  } = props;
+
   return (
-    <Box
-      display="flex"
-      width="100%"
-      justifyContent="space-between"
-      mt={4}
-    >
+    <Box display="flex" width="100%" justifyContent="space-between" mt={4}>
       <FormControlLabel
         label="In Progress"
+        onChange={(e) => onStatusChange(e)}
         control={<Switch color="warning" />}
       />
       <Button
@@ -18,6 +20,7 @@ const TaskFooter: FC = (): ReactElement => {
         color="success"
         size="small"
         sx={{ color: 'white' }}
+        onClick={(e) => onClick(e)}
       >
         Mark Complete
       </Button>
