8809165857ed1bd40eef5094d5587e0523d4c73b
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-02 15:00:55 -0700

    create an interface for the TaskDescription component

--- src/components/task/_TaskDescription.tsx
+++ src/components/task/_TaskDescription.tsx
@@ -1,7 +1,12 @@
 import React, { FC, ReactElement } from 'react';
+import { Box, Typography } from '@mui/material';
 
 const TaskDescription: FC = (): ReactElement => {
-  return <></>;
+  return (
+    <Box>
+      <Typography>Test description</Typography>
+    </Box>
+  );
 };
 
 export default TaskDescription;
