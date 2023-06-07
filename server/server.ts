281914e56490dfd3a150bf5ccf5a48ff3561ed87
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-07 14:41:02 -0700

    connect to the MySQL and create a todo  database

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,31 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
+import { DataSource } from 'typeorm';
 
 const app: Express = express();
 
-const port = process.env.PORT || 3200;
-
-app.use("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript");
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306, // default port
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true, // for development mode.
 });
 
-const start = async () => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-};
+const port = process.env.PORT || 3200;
 
-start();
+app.use('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript');
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port);
+    console.log('MySQL DB has been initialized');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
