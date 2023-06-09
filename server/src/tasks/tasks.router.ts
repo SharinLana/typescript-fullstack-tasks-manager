d607ef3368d08d5087204c423d57a1b92a441937
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-08 18:55:09 -0700

    refactor the post request in the tasks.router.ts

--- server/src/tasks/tasks.router.ts
+++ server/src/tasks/tasks.router.ts
@@ -1,23 +1,11 @@
-import { Router, Request, Response } from 'express';
-import { validationResult } from 'express-validator';
-import {taskController} from './tasks.controller';
+import { Router } from 'express';
+import { taskController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
 
 const tasksRouter: Router = Router();
 
 tasksRouter.get('/', taskController.getAll);
 
-tasksRouter.post(
-  '/task',
-  createValidator,
-  // @ts-ignore
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-  },
-);
+tasksRouter.post('/task', createValidator, taskController.addTask);
 
 export default tasksRouter;
