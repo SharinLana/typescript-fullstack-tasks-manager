628513749f1f7dd54af025b9cd8b8f7b1e502a7c
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-08 18:54:27 -0700

    code refactoring: create a method for a post request inside of the TaskController

--- server/src/tasks/tasks.controller.ts
+++ server/src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 import { Task } from './tasks.entity';
 import { AppDataSource } from '../../server';
 import { instanceToPlain } from 'class-transformer';
@@ -19,6 +20,35 @@ class TaskController {
       allTasks = instanceToPlain(allTasks) as Task[];
 
       return res.status(200).json(allTasks);
+    } catch (err) {
+      return res.status(500).json({ err: 'internal server error' });
+    }
+  }
+
+  public async addTask(req: Request, res: Response): Promise<Response> {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const newTask = new Task();
+
+    newTask.title = req.body.title;
+    newTask.date = req.body.date;
+    newTask.description = req.body.description;
+    newTask.priority = req.body.priority;
+    newTask.status = req.body.status;
+
+    let createdTask: Task;
+
+    try {
+      createdTask = await AppDataSource.getRepository(Task).save(newTask);
+
+      // Convert the task instance to a plain object
+      createdTask = instanceToPlain(createdTask) as Task;
+
+      return res.status(201).json(createdTask);
     } catch (err) {
       return res.status(500).json({ err: 'internal server error' });
     }
