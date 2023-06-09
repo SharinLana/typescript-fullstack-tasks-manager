422aeb88f8cbac4c5c1cd2f8775cc7723e143058
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-09 13:29:14 -0700

    create a put route for updating a task

--- server/src/tasks/tasks.router.ts
+++ server/src/tasks/tasks.router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { taskController } from './tasks.controller';
-import { createValidator } from './tasks.validator';
+import { createValidator, updateValidator } from './tasks.validator';
 
 const tasksRouter: Router = Router();
 
 tasksRouter.get('/', taskController.getAll);
 
 tasksRouter.post('/task', createValidator, taskController.addTask);
+tasksRouter.put('/task', updateValidator, taskController.updateTask);
 
 export default tasksRouter;
