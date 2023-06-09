e046eafa5e5776f43eb29770a33f8d0b5a2f61e7
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-09 13:00:24 -0700

    create an updateValidator to validate a put request

--- server/src/tasks/tasks.validator.ts
+++ server/src/tasks/tasks.validator.ts
@@ -33,3 +33,18 @@ export const createValidator: ValidationChain[] = [
     .isIn([Status.todo, Status.inProgress, Status.completed])
     .withMessage('Status can only be todo, inProgress or completed'),
 ];
+
+export const updateValidator = [
+  body('id')
+    .not()
+    .isEmpty()
+    .withMessage('The task id is mandatory')
+    .trim()
+    .isString()
+    .withMessage('Id needs to be a valid uuid format'),
+
+  body('status')
+    .trim()
+    .isIn([Status.todo, Status.inProgress, Status.completed])
+    .withMessage('Status can only be todo, inProgress or completed'),
+];
