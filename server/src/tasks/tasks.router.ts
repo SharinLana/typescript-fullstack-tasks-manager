892f5a9782f2e37f3d0698f9b66cfc72cb95b9e2
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-08 17:44:32 -0700

    check the result of validation in the post route

--- server/src/tasks/tasks.router.ts
+++ server/src/tasks/tasks.router.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 import { TaskController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
 
@@ -10,6 +11,17 @@ tasksRouter.get('/', async (req: Request, res: Response) => {
   res.status(200).json(allTasks);
 });
 
-tasksRouter.post('/task', createValidator, async (req: Request, res: Response) => {});
+tasksRouter.post(
+  '/task',
+  createValidator,
+  // @ts-ignore
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  },
+);
 
 export default tasksRouter;
